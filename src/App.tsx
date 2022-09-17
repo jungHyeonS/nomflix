1d9f85774a9d1bc50c3c0d89d116c2e32148e1fe
jungHyeonS <fbtls25@naver.com>
2022-09-17 13:44:45 +0900

    react-master-class header animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './Routes/Home';
+import Tv from './Routes/Tv';
+import Search from './Routes/Search';
+import Header from './Components/Header';
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path='/tv' element={<Tv/>}></Route>
+        <Route path='/serach' element={<Search/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
