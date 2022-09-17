21c91295740de1da0434083abad1a9fc586d201c
jungHyeonS <fbtls25@naver.com>
2022-09-17 12:58:26 +0900

    feat : project init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
+import {theme} from "./theme"
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const GlobalStyle = createGlobalStyle`
+`
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <ThemeProvider theme={theme}> 
+      <GlobalStyle/>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
